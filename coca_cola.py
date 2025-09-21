2e24b1686754d3061e66fe29b0afd26e5a514f3b
Andrew Merritt <a0merr05@louisville.edu>
2025-09-21 00:11:33 -0400

    coca_cola.py

---
+++ coca_cola.py
@@ -0,0 +1,5 @@
+coke_types = ["Coke Zero", "Diet Coke", "Coke"]
+flavors = ["Cherry", "Vanilla", "Plain"]
+for flavor in flavors:
+    for coke_type in coke_types:
+        print(f"{flavor}{coke_type}")
